8bd23a1a0aa95dda97de6051d87780255fa2195b
Kevin Haight <khaight@brooksbell.com>
2017-06-28 09:41:31 -0400

    added play audio only

--- app/controllers/play_audio_controller.rb
+++ app/controllers/play_audio_controller.rb
@@ -2,7 +2,8 @@ class PlayAudioController < ActionController::API
   def create
     case params[:request_type]
     when 'CallStart'
-      render json: { response_type: 'PlayAudio', audio_list: [url_for('audio/playaudioonly.m4a')] }
+      Rails.logger.debug(url_for('/audio/playaudioonly.m4a'))
+      render json: { response_type: 'PlayAudio', audio_list: [url_for('/audio/playaudioonly.m4a')] }
     when 'Termination'
     else
       Rails.logger.error('unable to get request type')
