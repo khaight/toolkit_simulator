9fe1e8788c4a5bf97cb982069660d8f2197dcaf8
Kevin Haight <khaight@brooksbell.com>
2017-06-28 10:04:52 -0400

    added speech

--- app/controllers/play_audio_speech_controller.rb
+++ app/controllers/play_audio_speech_controller.rb
@@ -3,7 +3,7 @@ class PlayAudioSpeechController < ActionController::API
     case params[:request_type]
     when 'CallStart'
       render json: { response_type: 'PlayAudio', speech: true, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiospeech.mp3'] }
-    when 'KeyPress'
+    when 'Speech'
       render json: { response_type: 'PlayAudio', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/responseaudio.mp3'] }
     when 'Termination'
     else
