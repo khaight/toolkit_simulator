a6478fbd1fc26cc103e0fa08f61967518f1952eb
Kevin Haight <khaight@brooksbell.com>
2017-06-28 09:47:38 -0400

    changed to mp3

--- app/controllers/play_audio_controller.rb
+++ app/controllers/play_audio_controller.rb
@@ -2,7 +2,7 @@ class PlayAudioController < ActionController::API
   def create
     case params[:request_type]
     when 'CallStart'
-      render json: { response_type: 'PlayAudio', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudioonly.m4a'] }
+      render json: { response_type: 'PlayAudio', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudioonly.mp3'] }
     when 'Termination'
     else
       Rails.logger.error('unable to get request type')
