edcdf701b566db923473d2d0575e613beb31499d
Kevin Haight <khaight@brooksbell.com>
2018-02-27 21:36:13 -0500

    added notifiy on timeout app

---
+++ app/controllers/play_audio_dtmf_notify_controller.rb
@@ -0,0 +1,13 @@
+class PlayAudioDtmfNotifyController < ActionController::API
+  def create
+    case params[:request_type]
+    when 'CallStart'
+      render json: { response_type: 'PlayAudio', dtmf: true, notify_on_timeout: true, num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiodtmf.mp3'] }
+    when 'KeyPress'
+      render json: { response_type: 'PlayAudio', notify_on_timeout: true, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/responseaudio.mp3'] }
+    when 'Termination'
+    else
+      Rails.logger.error('unable to get request type')
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :play_audio, only: %i[create]
   resources :play_audio_dtmf, only: %i[create]
   resources :play_audio_speech, only: %i[create]
+  resources :play_audio_dtmf_notify, only: %i[create]
 end
