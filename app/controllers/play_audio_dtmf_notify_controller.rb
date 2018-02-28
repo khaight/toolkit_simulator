59f0d999b1d1670a8f0fc1636dce04f37371f087
Kevin Haight <khaight@brooksbell.com>
2018-02-27 22:21:34 -0500

    added timeout hander

--- app/controllers/play_audio_dtmf_notify_controller.rb
+++ app/controllers/play_audio_dtmf_notify_controller.rb
@@ -5,6 +5,8 @@ class PlayAudioDtmfNotifyController < ActionController::API
       render json: { response_type: 'PlayAudio', dtmf: true, notify_on_timeout: true, num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiodtmf.mp3'] }
     when 'KeyPress'
       render json: { response_type: 'PlayAudio', notify_on_timeout: true, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/responseaudio.mp3'] }
+    when 'Timeout'
+      render json: { response_type: 'PlayAudio', dtmf: true, notify_on_timeout: true, num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiodtmf.mp3'] }
     when 'Termination'
     else
       Rails.logger.error('unable to get request type')
