64022d07c3a1fae31455a98f19665805ab208b80
Kevin Haight <khaight@brooksbell.com>
2018-02-27 22:29:34 -0500

    added timeout hander

--- app/controllers/play_audio_dtmf_notify_controller.rb
+++ app/controllers/play_audio_dtmf_notify_controller.rb
@@ -2,11 +2,11 @@ class PlayAudioDtmfNotifyController < ActionController::API
   def create
     case params[:request_type]
     when 'CallStart'
-      render json: { response_type: 'PlayAudio', dtmf: true, notify_on_timeout: true, num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiodtmf.mp3'] }
+      render json: { response_type: 'PlayAudio', dtmf: true, notify_no_input: true, num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiodtmf.mp3'] }
     when 'KeyPress'
-      render json: { response_type: 'PlayAudio', notify_on_timeout: true, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/responseaudio.mp3'] }
+      render json: { response_type: 'PlayAudio', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/responseaudio.mp3'] }
     when 'Timeout'
-      render json: { response_type: 'PlayAudio', dtmf: true, notify_on_timeout: true, num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiodtmf.mp3'] }
+      render json: { response_type: 'PlayAudio', notify_no_input: true, speech_hints: 'zoove, starstar, mobile', speech: true, timeout: 1, finish_on_key: '#', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiospeech.mp3'] }
     when 'Termination'
     else
       Rails.logger.error('unable to get request type')
