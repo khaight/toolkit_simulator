429b310d755606284731e2b792fbe6e103883b21
Kevin Haight <khaight@brooksbell.com>
2018-03-18 21:00:15 -0400

    added audio_loop

--- app/controllers/play_audio_dtmf_notify_controller.rb
+++ app/controllers/play_audio_dtmf_notify_controller.rb
@@ -2,7 +2,7 @@ class PlayAudioDtmfNotifyController < ActionController::API
   def create
     case params[:request_type]
     when 'CallStart'
-      render json: { response_type: 'PlayAudio', dtmf: true, notify_no_input: true, num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiodtmf.mp3', 'this is a first text to speach', 'this is a second text to speach'] }
+      render json: { response_type: 'PlayAudio', dtmf: true, notify_no_input: true, num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiodtmf.mp3'], audio_loop: 3 }
     when 'KeyPress'
       render json: { response_type: 'Forward', forward_to: '14155150440' }
     when 'NoInput'
