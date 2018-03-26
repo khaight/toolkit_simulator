0c2d6319d9669ced522a715cc621adb61e1f0e00
Kevin Haight <khaight@brooksbell.com>
2018-03-26 10:41:09 -0400

    changed transfer to connect

--- app/controllers/connect_controller.rb
+++ app/controllers/connect_controller.rb
@@ -1,8 +1,8 @@
-class TransferController < ActionController::API
+class ConnectController < ActionController::API
   def create
     case params[:request_type]
     when 'CallStart'
-      render json: { response_type: 'Transfer', forward_to: '14155150440' }
+      render json: { response_type: 'Connect', forward_to: '14155150440' }
     when 'Termination'
     else
       Rails.logger.error('unable to get request type')
