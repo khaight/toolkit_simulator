254c5e8da8966f7340cf938391f7c8bd8bccc8a1
Kevin Haight <khaight@brooksbell.com>
2018-04-26 21:27:36 -0400

    changed to gather after

--- app/controllers/play_audio_dtmf_controller.rb
+++ app/controllers/play_audio_dtmf_controller.rb
@@ -4,7 +4,7 @@ class PlayAudioDtmfController < ActionController::API
     when 'CallStart'
       render json: { response_type: 'PlayAudio', dtmf: true, num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiodtmf.mp3'] }
     when 'KeyPress'
-      render json: { response_type: 'PlayAudio', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/responseaudio.mp3'] }
+      render json: { response_type: 'PlayAudio', gather_after: true,  audio_list: ['https://toolkit-simulator.herokuapp.com/audio/responseaudio.mp3'] }
     when 'Termination'
     else
       Rails.logger.error('unable to get request type')
