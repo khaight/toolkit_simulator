9ddf3c967b128160bb2443f0f5bcc1dba5bee2f3
Kevin Haight <khaight@brooksbell.com>
2018-04-26 22:05:53 -0400

    updated speech

--- app/controllers/play_audio_speech_controller.rb
+++ app/controllers/play_audio_speech_controller.rb
@@ -2,7 +2,7 @@ class PlayAudioSpeechController < ActionController::API
   def create
     case params[:request_type]
     when 'CallStart'
-      render json: { response_type: 'PlayAudio', speech_hints: 'zoove, starstar, mobile', speech_timeout: 'auto', speech: true, timeout: 1, finish_on_key: '#', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiospeech.mp3'] }
+      render json: { response_type: 'PlayAudio', gather_after: true, speech_hints: 'zoove, starstar, mobile', speech_timeout: 'auto', speech: true, timeout: 1, finish_on_key: '#', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiospeech.mp3'] }
     when 'Speech'
       render json: { response_type: 'PlayAudio', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/responseaudio.mp3'] }
     when 'Termination'
