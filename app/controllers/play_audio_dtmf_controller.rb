e2ef070def80bb3a146a6790298fce80a813cbbe
Kevin Haight <khaight@brooksbell.com>
2018-04-26 21:34:32 -0400

    moved gather after to init play audio

--- app/controllers/play_audio_dtmf_controller.rb
+++ app/controllers/play_audio_dtmf_controller.rb
@@ -2,9 +2,9 @@ class PlayAudioDtmfController < ActionController::API
   def create
     case params[:request_type]
     when 'CallStart'
-      render json: { response_type: 'PlayAudio', dtmf: true, num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiodtmf.mp3'] }
+      render json: { response_type: 'PlayAudio', gather_after: true, dtmf: true, num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiodtmf.mp3'] }
     when 'KeyPress'
-      render json: { response_type: 'PlayAudio', gather_after: true,  audio_list: ['https://toolkit-simulator.herokuapp.com/audio/responseaudio.mp3'] }
+      render json: { response_type: 'PlayAudio', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/responseaudio.mp3'] }
     when 'Termination'
     else
       Rails.logger.error('unable to get request type')
