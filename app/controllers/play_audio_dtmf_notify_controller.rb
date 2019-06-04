caad13a4b294ca6e3a520f811adeaa60d8fab8e3
Kevin Haight <kevinjhaight@gmail.com>
2019-06-04 08:56:23 -0400

    updates to change values to string

--- app/controllers/play_audio_dtmf_notify_controller.rb
+++ app/controllers/play_audio_dtmf_notify_controller.rb
@@ -2,7 +2,7 @@ class PlayAudioDtmfNotifyController < ActionController::API
   def create
     case params[:request_type]
     when 'CallStart'
-      render json: { response_type: 'PlayAudio', dtmf: true, notify_no_input: true, num_digits: 1, audio_url: 'https://toolkit-simulator.herokuapp.com/audio/playaudiodtmf.mp3', audio_loop: 3 }
+      render json: { response_type: 'PlayAudio', dtmf: 'true', notify_no_input: 'true', num_digits: 1, audio_url: 'https://toolkit-simulator.herokuapp.com/audio/playaudiodtmf.mp3', audio_loop: 3 }
     when 'KeyPress'
       render json: { response_type: 'Forward', forward_to: '14155150440' }
     when 'NoInput'
