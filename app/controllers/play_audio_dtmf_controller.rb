a9304f6cc2b129e794d909dc173cf4f6a21e6708
Kevin Haight <kevinjhaight@gmail.com>
2019-06-04 09:11:14 -0400

    turn off gather after

--- app/controllers/play_audio_dtmf_controller.rb
+++ app/controllers/play_audio_dtmf_controller.rb
@@ -2,7 +2,7 @@ class PlayAudioDtmfController < ActionController::API
   def create
     case params[:request_type]
     when 'CallStart'
-      render json: { response_type: 'PlayAudio', gather_after: 'true', dtmf: 'true', num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiodtmf.mp3'] }
+      render json: { response_type: 'PlayAudio', gather_after: 'false', dtmf: 'true', num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/playaudiodtmf.mp3'] }
     when 'KeyPress'
       render json: { response_type: 'PlayAudio', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/responseaudio.mp3'] }
     when 'Termination'
