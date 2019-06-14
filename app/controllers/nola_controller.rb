138a1485f9ef4c6af6546fbd30138cf17bae6078
Kevin Haight <kevinjhaight@gmail.com>
2019-06-14 12:26:41 -0400

    convert to mp3

--- app/controllers/nola_controller.rb
+++ app/controllers/nola_controller.rb
@@ -2,9 +2,9 @@ class NolaController < ActionController::API
   def create
     case params[:request_type]
     when 'CallStart'
-      render json: { response_type: 'PlayAudio', gather_after: 'false', dtmf: 'true', num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/nola.m4a'] }
+      render json: { response_type: 'PlayAudio', gather_after: 'false', dtmf: 'true', num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/nola.mp3'] }
     when 'KeyPress'
-      render json: { response_type: 'PlayAudio', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/nola.m4a'] }
+      render json: { response_type: 'PlayAudio', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/nola.mp3'] }
     when 'Termination'
     else
       Rails.logger.error('unable to get request type')
