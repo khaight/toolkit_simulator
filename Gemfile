3237de89a0d0990a5f6c2faae375e173754a6c13
Kevin Haight <kevinjhaight@gmail.com>
2019-10-21 17:18:42 -0400

    updates to nola

--- Gemfile
+++ Gemfile
@@ -1,20 +1,19 @@
 source 'https://rubygems.org'
 
 git_source(:github) do |repo_name|
-  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
+  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?('/')
   "https://github.com/#{repo_name}.git"
 end
 
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.3'
 gem 'puma', '~> 3.0'
+gem 'rails', '~> 5.0.3'
+gem 'rest-client', '~> 2.0.2'
 
 group :development do
   gem 'listen', '~> 3.1.5'
   gem 'rubocop', '~> 0.48.1'
 end
 
-
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
--- app/controllers/nola_controller.rb
+++ app/controllers/nola_controller.rb
@@ -2,12 +2,38 @@ class NolaController < ActionController::API
   def create
     case params[:request_type]
     when 'CallStart'
-      render json: { response_type: 'PlayAudio', gather_after: 'false', dtmf: 'true', num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/nola.wav'] }
+      render json: { response_type: 'PlayAudio', gather_after: 'false', dtmf: 'true', num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/nola/main.wav'] }
+      send_message(params[:mdn])
     when 'KeyPress'
-      render json: { response_type: 'PlayAudio', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/nola_legal.wav'] }
+      if params[:key_pressed].to_i == 1
+        render json: { response_type: 'PlayAudio', dtmf: 'false', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/nola/song.wav'] }
+      elsif params[:key_pressed].to_i == 2
+        render json: { response_type: 'PlayAudio', dtmf: 'false', audio_list: ['https://toolkit-simulator.herokuapp.com/audio/nola/legal;.wav'] }
+      else
+        render json: { response_type: 'PlayAudio', gather_after: 'false', dtmf: 'true', num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/nola/main.wav'] }
+      end
     when 'Termination'
+    when 'NoInput'
+      render json: { response_type: 'PlayAudio', gather_after: 'false', dtmf: 'true', num_digits: 1, audio_list: ['https://toolkit-simulator.herokuapp.com/audio/nola/main.wav'] }
     else
       Rails.logger.error('unable to get request type')
     end
+  end
+
+  private
+
+  def send_message(mdn)
+    payload = { number: '**NOLA', type: 'sms', mdn: mdn,
+                msgtext: "Thank you for your call. I hope you love my new EP, Medicine. Stream it here:\nSPOTIFY: http://bit.ly/nolaspotify\nAPPLE MUSIC: http://bit.ly/nolaapplemusic\nSoundcloud: http://bit.ly/nolasoundcloud" }
+    resp = RestClient::Request.execute(method: :post, url: 'https://api.starstarmobile.com/shortdial/v1/16534f8049900135f7dc009c029b1b40/messages', payload: payload.to_json, headers: api_headers, timeout: 5)
+    Rails.logger.info('Message Response' + resp.inspect)
+  end
+
+  def api_headers
+    headers = {}
+    headers['Content-Type'] = 'application/json'
+    headers['Accept'] = 'application/json'
+    headers['master-account-id'] = '243e2d25037175ab22b099920773bf94'
+    headers
   end
 end
